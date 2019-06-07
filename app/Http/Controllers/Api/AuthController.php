4e34e3b64cecf597c2e62010d86509963afc1f1e
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-07 07:53:25 -0400

    Create login method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,4 +29,23 @@ class AuthController extends Controller
 
         return response($response, 200);
     }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = ['msg' => 'Password missmatch'];
+                return response($response, 422);
+            }
+        } else {
+            $response = ['msg' => 'User does not exist'];
+            return response($response, 422);
+        }
+    }
 }
