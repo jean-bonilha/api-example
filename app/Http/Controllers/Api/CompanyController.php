d3c86de2dddb2382633e78ee995a90b497f6bd52
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-14 16:32:24 -0400

    Removes methods that showing forms

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -19,16 +19,6 @@ class CompanyController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -46,17 +36,6 @@ class CompanyController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Company $company)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Company  $company
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Company $company)
     {
         //
     }
