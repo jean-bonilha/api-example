241ce58c0f910aafe2c2d0648ddd88e7725cc81b
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-17 15:33:03 -0400

    Change all routes for AuthController

--- routes/app_api.v1.php
+++ routes/app_api.v1.php
@@ -1,12 +1,12 @@
 <?php
 
 // public routes
-Route::post('/login', 'AuthController@login')->name('login');
-Route::post('/register', 'AuthController@register')->name('register');
+Route::post('/auth/login', 'AuthController@login')->name('login');
+Route::post('/auth/register', 'AuthController@register')->name('register');
 
 // private routes
 Route::middleware('auth:api')->group(function () {
-    Route::get('/logout', 'AuthController@logout')->name('logout');
+    Route::get('/auth/logout', 'AuthController@logout')->name('logout');
     Route::get('/auth/user', 'AuthController@user')->name('user');
     Route::apiResources([
         'user' => 'UserController',
