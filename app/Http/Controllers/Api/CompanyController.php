54d4dbbb1586c14d1ce41b2c1182cc8e1499c6f5
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-21 14:24:49 -0400

    Removes items for validations

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -14,9 +14,6 @@ class CompanyController extends Controller
             'razao_social' => 'string|min:3|max:255',
             'nome_fantasia' => 'string|min:3|max:255',
             'cnpj' => 'string|min:18|max:18',
-            'codigo_descricao' => 'string|min:3',
-            'grau_risco' => 'string|min:2|max:2',
-            'grupo_risco' => 'string|min:5|max:5',
         ]);
     }
 }
