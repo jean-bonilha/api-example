569fa20d82eeed6024850fc342f7c5f16d70ea8a
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-24 08:59:50 -0400

    Adds saved_user column on users, companies and people tables

---
+++ database/migrations/2019_06_13_124018_add_saved_user_column_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSavedUserColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('saved_user')->unsigned()->nullable();
+            $table->foreign('saved_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('saved_user');
+        });
+    }
+}
--- database/migrations/2019_06_17_202808_create_people_table.php
+++ database/migrations/2019_06_17_202808_create_people_table.php
@@ -36,7 +36,7 @@ class CreatePeopleTable extends Migration
             $table->string('nome_pai')->nullable();
             $table->string('ctps_numero')->nullable();
             $table->string('ctps_serie')->nullable();
-            $table->bigInteger('saved_user')->unsigned();
+            $table->bigInteger('saved_user')->unsigned()->nullable();
             $table->foreign('saved_user')->references('id')->on('users');
             $table->timestamps();
         });
