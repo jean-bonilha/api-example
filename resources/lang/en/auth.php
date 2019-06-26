1fc918b223fab8ce10e8da80f6b7ea83a1cdea81
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-26 16:19:07 -0400

    Adds items to auth language resource

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,13 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'unauthorizated' => 'Unauthorizated.',
+    'created' => 'User created successfully!',
+    'no-activated' => 'User not activated.',
+    'firebase-save-failed' => 'Save user on Firebase service failed.',
+    'firebase-login-failed' => 'User logout on Firebase service failed.',
+    'password-mismatch' => 'Password mismatch.',
+    'user-not-found' => 'User does not exist.',
+    'login-success' => 'You have been successfully logged out.',
 
 ];
--- resources/lang/pt-BR/auth.php
+++ resources/lang/pt-BR/auth.php
@@ -15,5 +15,13 @@ return [
 
     'failed' => 'Essas credenciais não correspondem aos nossos registros.',
     'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+    'unauthorizated' => 'Não autorizado.',
+    'created' => 'Usuário criado com sucesso!',
+    'no-activated' => 'O usuário não foi ativado.',
+    'firebase-save-failed' => 'Salvar usuário no serviço do Firebase falhou.',
+    'firebase-login-failed' => 'O logout do usuário no serviço do Firebase falhou.',
+    'password-mismatch' => 'Senha incorreta.',
+    'user-not-found' => 'Usuário não existe.',
+    'login-success' => 'Você foi desconectado com sucesso.',
 
 ];
