d24bc9a7576cd70be6f74b01e48921654951932d
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-06-28 08:52:14 -0400

    Cunstomizeds UserResource and UserCollection

--- app/Http/Resources/Api/v1/UserCollection.php
+++ app/Http/Resources/Api/v1/UserCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Api\v1;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\User;
 
 class UserCollection extends ResourceCollection
 {
@@ -14,6 +15,16 @@ class UserCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection->transform(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'email' => $item->email,
+                    'registered_by' => $item->registered_by ? User::find($item->registered_by)->name : 'AUTO CADASTRO',
+                    'activated' => $item->activated ? 'ATIVADO' : 'DESATIVADO',
+                ];
+            }),
+        ];
     }
 }
--- app/Http/Resources/Api/v1/UserResource.php
+++ app/Http/Resources/Api/v1/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Api\v1;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\User;
 
 class UserResource extends JsonResource
 {
@@ -14,6 +15,12 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'registered_by' => $this->registered_by ? User::find($this->registered_by)->name : 'AUTO CADASTRO',
+            'activated' => $this->activated ? 'ATIVADO' : 'DESATIVADO',
+        ];
     }
 }
