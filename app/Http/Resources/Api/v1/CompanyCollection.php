8cf6f7f5c0a746b77c59213448ce03120ac32831
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-10 09:42:46 -0400

    Alters fields from CompanyCollection and CompanyResource

--- app/Http/Resources/Api/v1/CompanyCollection.php
+++ app/Http/Resources/Api/v1/CompanyCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Api\v1;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\User;
 
 class CompanyCollection extends ResourceCollection
 {
@@ -15,7 +16,19 @@ class CompanyCollection extends ResourceCollection
     public function toArray($request)
     {
         return [
-            'data' => $this->collection,
+            'data' => $this->collection->transform(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'razao_social' => $item->razao_social,
+                    'nome_fantasia' => $item->nome_fantasia,
+                    'cnpj' => $item->cnpj,
+                    'codigo_descricao' => $item->codigo_descricao,
+                    'grau_risco' => $item->grau_risco,
+                    'grupo_risco' => $item->grupo_risco,
+                    'registered_by' => $item->registered_by ? User::find($item->registered_by)->name : 'AUTO CADASTRO',
+                    'form' => 'company-form',
+                ];
+            }),
         ];
     }
 }
--- app/Http/Resources/Api/v1/CompanyResource.php
+++ app/Http/Resources/Api/v1/CompanyResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Resources\Api\v1;
+use App\User;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -22,6 +23,10 @@ class CompanyResource extends JsonResource
             'codigo_descricao' => $this->codigo_descricao,
             'grau_risco' => $this->grau_risco,
             'grupo_risco' => $this->grupo_risco,
+            'registered_by' => $this->registered_by,
+            'registered_by_name' => $this->registered_by ? User::find($this->registered_by)->name : 'AUTO CADASTRO',
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
