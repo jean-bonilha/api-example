46a1a67070fcff6e673065bb8be5ba308d308926
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-16 09:06:32 -0400

    Sets fillable array of Person model

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -5,11 +5,34 @@ namespace App\Models;
 class Person extends BaseModel
 {
     /**
-     * The attributes that aren't mass assignable.
+     * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $guarded = [];
+    protected $fillable = [
+        'nome',
+        'rg',
+        'data_nascimento',
+        'sexo',
+        'rg_orgao_uf_emissao',
+        'rg_data_expedicao',
+        'cpf',
+        'cnh',
+        'cnh_categoria',
+        'nis',
+        'raca_cor',
+        'estado_civil',
+        'grau_instrucao',
+        'nome_social',
+        'minicipio',
+        'uf',
+        'codigo_pais_nascimento',
+        'nome_mae',
+        'nome_pai',
+        'ctps_numero',
+        'ctps_serie',
+        'registered_by',
+    ];
 
     /**
      * The name of field for filter
