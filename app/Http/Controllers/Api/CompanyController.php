bac5ba2bdd51740f05e0c8c10e22c27eeb56b97d
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-18 15:21:26 -0400

    Removes codigo_descricao validation

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -13,7 +13,6 @@ class CompanyController extends Controller
             'razao_social' => 'required|string|min:3|max:255',
             'nome_fantasia' => 'required|string|min:3|max:255',
             'cnpj' => 'required|string|min:18|max:18|unique:companies',
-            'codigo_descricao' => 'string|min:3',
             'grau_risco' => 'string|min:2|max:2',
             'grupo_risco' => 'string|min:5|max:5',
         ]);
