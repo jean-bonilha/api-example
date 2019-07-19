b41320b3c2703b6541cfa1428b5a30451c8f49b3
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 14:28:32 -0400

    Sets fillable and filterBy attribute from Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,5 +4,23 @@ namespace App\Models;
 
 class Employee extends BaseModel
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'company_id',
+        'person_id',
+        'sector_id',
+        'role_id',
+        'registered_by',
+    ];
+
+    /**
+     * The name of field for filter
+     *
+     * @var string
+     */
+    protected $filterBy = 'company_id';
 }
