48278a69aec778c90c3f99f1e99ac65704e45510
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 11:20:19 -0400

    Sets EmployeeFactory fields in define method

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Employee::class, function (Faker $faker) {
     return [
-        //
+        'company_id' => DB::table('companies')->inRandomOrder()->get()->id,
+        'person_id' => DB::table('people')->inRandomOrder()->get()->id,
+        'sector_id' => DB::table('sectors')->inRandomOrder()->get()->id,
+        'role_id' => DB::table('roles')->inRandomOrder()->get()->id,
+        'registered_by' => DB::table('users')->orderBy('id', 'desc')->first()->id,
     ];
 });
