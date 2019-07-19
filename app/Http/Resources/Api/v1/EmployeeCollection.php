b05dae8d31fbeaf3e8ee39b4c46000a7d1f3751c
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 15:48:25 -0400

    Sets name of each foreign key in table

--- app/Http/Resources/Api/v1/EmployeeCollection.php
+++ app/Http/Resources/Api/v1/EmployeeCollection.php
@@ -3,6 +3,10 @@
 namespace App\Http\Resources\Api\v1;
 
 use App\User;
+use App\Models\Company;
+use App\Models\Person;
+use App\Models\Sector;
+use App\Models\Role;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class EmployeeCollection extends ResourceCollection
@@ -20,9 +24,13 @@ class EmployeeCollection extends ResourceCollection
                 return [
                     'id' => $item->id,
                     'company_id' => $item->company_id,
+                    'company_name' => Company::find($item->company_id)->razao_social,
                     'person_id' => $item->person_id,
+                    'person_name' => Person::find($item->person_id)->nome,
                     'sector_id' => $item->sector_id,
+                    'sector_name' => Sector::find($item->sector_id)->nome,
                     'role_id' => $item->role_id,
+                    'role_name' => Role::find($item->role_id)->nome,
                     'registered_by' => $item->registered_by ? User::find($item->registered_by)['name'] : 'AUTO CADASTRO',
                 ];
             }),
