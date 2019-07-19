5969ef8915cc88c1402d616239978f202526fa68
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 16:23:45 -0400

    Override index method EmployeeController class

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 use App\Http\Controllers\BaseController as Controller;
 
 class EmployeeController extends Controller
@@ -16,5 +18,45 @@ class EmployeeController extends Controller
             'role_id' => 'integer',
             'registered_by' => 'integer',
         ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $this->setResources();
+
+        $modelResource = new $this->Model;
+
+        $filter = Input::get('filter');
+
+        $perPage = Input::get('per_page');
+
+        $sort = Input::get('sort');
+
+        if ($perPage) $modelResource->setPerPage($perPage);
+
+        $filterBy = $modelResource->getFilterBy();
+
+        $sortBy = $modelResource->getSortBy() ?: $filterBy;
+
+        $sort = $sort ? explode('|', $sort) : [$sortBy, 'asc'];
+
+        if (!$filter) {
+            return new $this->ResourceCollection(
+                $modelResource::orderBy($sort[0], $sort[1])->paginate()
+            );
+        }
+
+        $filter = DB::table('companies')->where('razao_social', 'like', "%$filter%")->first()->id;
+
+        return new $this->ResourceCollection(
+            $modelResource::where("$filterBy", "$filter")
+                ->orderBy($sort[0], $sort[1])
+                ->paginate()
+        );
     }
 }
