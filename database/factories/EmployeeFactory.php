1961b79e356692fb3b04dbb5e6e7c19d771f8c2a
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 08:58:59 -0400

    Create Employee controller, model, factory and migration

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Employee;
+use Faker\Generator as Faker;
+
+$factory->define(Employee::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
