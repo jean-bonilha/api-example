ea09618388dd7cf7944ce1e9fe2de7eae62d69d4
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 10:40:11 -0400

    Sets Role and Sector Factory

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Role::class, function (Faker $faker) {
     return [
-        //
+        'nome' => $faker->name,
+        'registered_by' => DB::table('users')->orderBy('id', 'desc')->first()->id,
     ];
 });
