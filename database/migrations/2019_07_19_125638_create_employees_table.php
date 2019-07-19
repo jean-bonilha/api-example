ef3fa4c7002661094102f4363d0ede402bdfd019
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 09:39:13 -0400

    Implements foreign key fields on create_employees_table

--- database/migrations/2019_07_19_125638_create_employees_table.php
+++ database/migrations/2019_07_19_125638_create_employees_table.php
@@ -15,6 +15,22 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->bigInteger('company_id')->unsigned()->nullable();
+            $table->foreign('company_id')->references('id')->on('companies');
+
+            $table->bigInteger('person_id')->unsigned()->nullable();
+            $table->foreign('person_id')->references('id')->on('people');
+
+            $table->bigInteger('sector_id')->unsigned()->nullable();
+            $table->foreign('sector_id')->references('id')->on('sectors');
+
+            $table->bigInteger('role_id')->unsigned()->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
+
+            $table->bigInteger('registered_by')->unsigned()->nullable();
+            $table->foreign('registered_by')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
