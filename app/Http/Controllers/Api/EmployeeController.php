9dd5adf2fd55322674cced9e168c278e44b30554
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-19 14:09:25 -0400

    Sets EmployeeController how exetend BaseController

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -2,84 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Employee;
-use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController as Controller;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Employee  $employee
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Employee $employee)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Employee  $employee
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Employee $employee)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Employee  $employee
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Employee $employee)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Employee  $employee
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Employee $employee)
-    {
-        //
+        parent::__construct('Employee');
+        parent::setValidateFields([
+            'company_id' => 'integer',
+            'person_id' => 'integer',
+            'sector_id' => 'integer',
+            'role_id' => 'integer',
+            'registered_by' => 'integer',
+        ]);
     }
 }
