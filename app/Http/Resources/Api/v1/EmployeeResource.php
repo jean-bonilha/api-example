da6430916d65f6da66a1413be6b72755e8ad0e06
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-20 11:25:00 -0400

    Adds other fields on EmployeeResource

--- app/Http/Resources/Api/v1/EmployeeResource.php
+++ app/Http/Resources/Api/v1/EmployeeResource.php
@@ -3,6 +3,10 @@
 namespace App\Http\Resources\Api\v1;
 
 use App\User;
+use App\Models\Company;
+use App\Models\Person;
+use App\Models\Sector;
+use App\Models\Role;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EmployeeResource extends JsonResource
@@ -18,9 +22,13 @@ class EmployeeResource extends JsonResource
         return [
             'id' => $this->id,
             'company_id' => $this->company_id,
+            'company_name' => Company::find($this->company_id)->razao_social,
             'person_id' => $this->person_id,
+            'person_name' => Person::find($this->person_id)->nome,
             'sector_id' => $this->sector_id,
+            'sector_name' => Sector::find($this->sector_id)->nome,
             'role_id' => $this->role_id,
+            'role_name' => Role::find($this->role_id)->nome,
             'registered_by' => $this->registered_by,
             'registered_by_name' => $this->registered_by ? User::find($this->registered_by)['name'] : 'AUTO CADASTRO',
             'created_at' => $this->created_at,
