a4183c71b5a1f8875f10391fabb20d9e9910421a
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2019-07-26 14:26:37 -0400

    Removes grau_risco and grupo_risco from CompanyController

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -13,8 +13,6 @@ class CompanyController extends Controller
             'razao_social' => 'required|string|min:3|max:255',
             'nome_fantasia' => 'required|string|min:3|max:255',
             'cnpj' => 'required|string|min:18|max:18|unique:companies',
-            'grau_risco' => 'string|min:2|max:2',
-            'grupo_risco' => 'string|min:5|max:5',
         ]);
     }
 }
